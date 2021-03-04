a600890027dc014072b87b2c5ac18b759ceee5d7
Chas Johson <chasjohnson09@gmail.com>
2021-03-04 09:07:47 -0500

    Add project files.

---
+++ OderAppWebAPI/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace OderAppWebAPI.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        [StringLength(10), Required]
+        public string Code { get; set; }
+        [StringLength(50), Required]
+        public string Name { get; set; }
+        public bool IsNational { get; set; }
+        [Column(TypeName = "Decimal(9,2)")]
+        public decimal Sales { get; set; }
+        public DateTime? Created { get; set; }
+    }
+}
